0458e0eb783f8d1d1ba3d864e5c18aef8de14438
Murad Farhat <92052407+muradfarhat@users.noreply.github.com>
2023-07-03 20:40:27 +0300

    Card View

--- FilmXpress/Views/MovieCardTableViewCell.swift
+++ FilmXpress/Views/MovieCardTableViewCell.swift
@@ -15,10 +15,20 @@ class MovieCardTableViewCell: UITableViewCell {
         return UINib(nibName: "MovieCardTableViewCell", bundle: nil)
     }
 
-    @IBOutlet weak var moviePoster: UIImageView!
-    @IBOutlet weak var cardView: UIView!
-    @IBOutlet weak var statusView: UIView!
-    @IBOutlet weak var scheduleView: UIView!
+    @IBOutlet weak private var moviePoster: UIImageView!
+    @IBOutlet weak private var cardView: UIView!
+    @IBOutlet weak private var statusView: UIView!
+    @IBOutlet weak private var scheduleView: UIView!
+    
+    @IBOutlet private var weekDays: Array<UILabel>!
+    
+    @IBOutlet weak private var movieTime: UILabel!
+    @IBOutlet weak private var movieFavorite: UIImageView!
+    @IBOutlet weak private var movieStatus: UILabel!
+    @IBOutlet weak private var movieType: UILabel!
+    @IBOutlet weak private var movieName: UILabel!
+    
+    private var days: [String] = []
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,5 +36,30 @@ class MovieCardTableViewCell: UITableViewCell {
         self.moviePoster.layer.cornerRadius = 20
         self.statusView.layer.cornerRadius = 15
         self.scheduleView.layer.cornerRadius = 15
+        
+        for day in weekDays {
+            day.textColor = UIColor.darkGray
+        }
+        
+        for day in days {
+            switch (day) {
+                case "Saturday":
+                    weekDays[0].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                case "Sunday":
+                    weekDays[1].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                case "Monday":
+                    weekDays[2].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                case "Tuesday":
+                    weekDays[3].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                case "Wednesday":
+                    weekDays[4].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                case "Thursday":
+                    weekDays[5].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                case "Friday":
+                    weekDays[6].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                default:
+                    continue
+            }
+        }
     }
 }
