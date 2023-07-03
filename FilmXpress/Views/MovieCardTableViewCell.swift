02ff4311691386ded3a69a26f8cba20d91cc73de
Murad Farhat <92052407+muradfarhat@users.noreply.github.com>
2023-07-03 22:28:43 +0300

    Card View Shadow

--- FilmXpress/Views/MovieCardTableViewCell.swift
+++ FilmXpress/Views/MovieCardTableViewCell.swift
@@ -28,16 +28,36 @@ class MovieCardTableViewCell: UITableViewCell {
     @IBOutlet weak private var movieType: UILabel!
     @IBOutlet weak private var movieName: UILabel!
     
-    private var days: [String] = ["Sunday", "Friday"]
+    private var days: [String] = ["Sunday", "Friday", "Wednesday"]
+    private let customColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)  // 0x29DBFF
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        self.setViewsRadius()
+        self.setViewShadow(view: self.cardView, color: self.customColor.cgColor, radius: 20, opacity: 0.3)
+        self.setDays()
+    }
+    
+    // private method to set radius for card views
+    private func setViewsRadius() {
         self.cardView.layer.cornerRadius = 20
         self.moviePoster.layer.cornerRadius = 20
         self.statusView.layer.cornerRadius = 15
         self.statusView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
-        self.scheduleView.layer.cornerRadius = 15
-        
+        self.scheduleView.layer.cornerRadius = 20
+    }
+    
+    // private method to set shadow for any custom view
+    private func setViewShadow(view: UIView, color: CGColor, radius: CGFloat, opacity: Float) {
+        view.layer.shadowColor = color
+        view.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        view.layer.shadowRadius = radius
+        view.layer.shadowOpacity = opacity
+    }
+    
+    // private method to set days color based on JSON data
+    private func setDays() {
         for day in weekDays {
             day.textColor = UIColor.darkGray
         }
@@ -45,19 +65,19 @@ class MovieCardTableViewCell: UITableViewCell {
         for day in days {
             switch (day) {
                 case "Saturday":
-                    weekDays[0].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                    weekDays[0].textColor = self.customColor
                 case "Sunday":
-                    weekDays[1].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                    weekDays[1].textColor = self.customColor
                 case "Monday":
-                    weekDays[2].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                    weekDays[2].textColor = self.customColor
                 case "Tuesday":
-                    weekDays[3].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                    weekDays[3].textColor = self.customColor
                 case "Wednesday":
-                    weekDays[4].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                    weekDays[4].textColor = self.customColor
                 case "Thursday":
-                    weekDays[5].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                    weekDays[5].textColor = self.customColor
                 case "Friday":
-                    weekDays[6].textColor = UIColor(red: 0x29/255, green: 0xDB/255, blue: 0xFF/255, alpha: 1.0)
+                    weekDays[6].textColor = self.customColor
                 default:
                     continue
             }
